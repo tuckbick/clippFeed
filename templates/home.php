5eaf29668d00fbc7b6e8e7d50b53f5078a203426
Tucker Bickler <Tucker@w-central-232-116.public.utexas.edu>
2010-10-16 10:18:06 -0500

    added search bar, made side bar wider

--- templates/home.php
+++ templates/home.php
@@ -1,18 +1,25 @@
 <?php
 include 'layout.php';
 
+$q = isset($_GET['q'])?$_GET['q']:'Search';
 $u = isset($_GET['u'])?$_GET['u']:'paste a URL of a video and click add';
 
 startblock('content'); 
 ?>
 
 <div id="home">
-	<div id="left_bar">
-		loading...
-	</div>
-	<div id="middle">
+    <div id="side_area">
+        <form id="search_bar">
+            <label class="visuallyhidden" for="search">Search</label>
+            <input id="search" name="q" type="text" value="<?php echo $q ?>" />
+        </form>
+        <div id="left_bar">
+            loading...
+        </div>
+    </div>
+    <div id="middle">
         <form id="add_clip">
-        	<label class="visuallyhidden" for="new_url">New Video URL</label>
+            <label class="visuallyhidden" for="new_url">New Video URL</label>
             <input id="new_url" name="url" type="text" value="<?php echo $u ?>" />
             <input type="submit" id="add_submit" value="Add" />
         </form>
@@ -27,25 +34,24 @@ startblock('scriptTag'); ?>
 <?php endblock(); 
 
 startblock('readyScript'); ?>
-	$.ajax({
-		url:'handler.php',
-		dataType:'json',
-		cache:'false',
-		data: {
-			action: 'getFeed',
-			uid: 0
-		},
-		success: function( data ) {
-		
-			var ret ='<ul>';
-			for(var i in data) {
-				ret += '<li><a href="javascript:getVideo('+ data[i].cid +')">'+ data[i].c_title +'</a></li>';
-			}
-			ret += "<ul>";
-			$('#left_bar').html(ret);
-		}
-	});
-	
+    $.ajax({
+        url:'handler.php',
+        dataType:'json',
+        cache:'false',
+        data: {
+            action: 'getFeed'
+        },
+        success: function( data ) {
+        
+            var ret ='<ul>';
+            for(var i in data) {
+                ret += '<li><a href="javascript:getVideo('+ data[i].cid +')">'+ data[i].c_title +'</a></li>';
+            }
+            ret += "<ul>";
+            $('#left_bar').html(ret);
+        }
+    });
+    
 <?php
 endblock();
 ?>
