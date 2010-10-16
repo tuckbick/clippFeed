0ac6868013897d7bf51cd49920db48ea09391e6c
Paul Marbach <fastfrwrd@wireless-128-62-82-75.public.utexas.edu>
2010-10-16 00:12:23 -0500

    fixed handler

--- handler.php
+++ handler.php
@@ -1,5 +1,7 @@
 <?php 
 
+header('Content-type: application/json');
+
 include_once "connect.php";
 include_once "functions.inc";
 include_once "functions2.inc";
@@ -7,12 +9,15 @@ include_once "functions2.inc";
 $result = "<p>No result.</p>";
 switch($_GET['action']) {
 	case "preview":
-		if(isset($_GET['vid'])&&isset($_GET['sid'])) {
-			$result = get_embed($_GET['vid'],$_GET['sid'],300,225);
+		if(isset($_GET['url'])&&isset($_GET['sid'])) {
+			$result = get_embed($_GET['url'],$_GET['sid'],300,225);
 		} else {
 			$result = "<p>please send me a video link!</p>";
 		}
+		$return['embed'] = $result;
 		break;
 }
+
+echo json_encode($return);
 
 ?>
