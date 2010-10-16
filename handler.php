1157ececa51d443a3cd1b86b09d88ff4993cc11a
Paul Marbach <fastfrwrd@wireless-128-62-82-75.public.utexas.edu>
2010-10-16 01:07:32 -0500

    fixed handler problems for preview

--- handler.php
+++ handler.php
@@ -9,7 +9,7 @@ include_once "functions2.inc";
 $result = "<p>No result.</p>";
 switch($_GET['action']) {
 	case "preview":
-		if(isset($_GET['url']) && isset($_GET['sid']) && extract_vid($_GET['url'])!=0) {
+		if(isset($_GET['url']) && isset($_GET['sid']) && extract_vid($_GET['url'],$_GET['sid'])!='0') {
 			$result = get_embed($_GET['url'],$_GET['sid'],300,225);
 		} else {
 			$result = "<p>please send me a video link!</p>";
