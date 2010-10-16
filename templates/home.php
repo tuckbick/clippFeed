e1ab1a14bfec839f61b3ac9058c657fe4df08c74
Tucker Bickler <Tucker@w-central-232-116.public.utexas.edu>
2010-10-15 23:11:30 -0500

    merge. added javascript for adding new video

--- templates/home.php
+++ templates/home.php
@@ -4,7 +4,48 @@ include 'layout.php';
 startblock('content'); ?>
 
 <div id="home">
-    home
+    <label for="new_url">New URL</label>
+    <input id="new_url" name="url" type="text" />
+    <div id="new_url_preview">
+        hello
+    </div>
 </div>
+
+<?php endblock();
+
+startblock('readyScript'); ?>
+
+    var exps = [
+        /^http:\/\/(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:\S+)?/,
+        /^http:\/\/(?:www\.)?vimeo.com(\/|\/clip:)(\d+)(.*?)/,
+        /^http:\/\/(?:www\.)?video.yahoo.com\/watch\/(.*?)/
+        //http://video.google.it/videoplay?docid=3947293349740494460
+        ///http:\/\/(www.vimeo|vimeo)\.com(\/|\/clip:)(\d+)(.*?)/,
+        //http://purinaanimalallstars.yahoo.com/?v=8380138
+        //
+    ],
+    validate = function( url ) {
+        for( var i in exps ) {
+            if (url.match(exps[i][0])) return i;
+        }
+        return -1;
+    },
+    $url = $('#new_url'),
+    $preview = $('#new_url_preview');
+    $url.bind('keyup paste', function(e) {
+        var url = $url.val(),
+            type = validate( url );
+        if( type != -1 ) {
+            $preview.slideDown( 'fast', function() {
+                $.getJSON('handler.php', {
+                    action: 'preview',
+                    sid: type,
+                    url: url
+                })
+            });
+        } else {
+            $preview.slideUp( 'fast' );
+        }
+    });
 
 <?php endblock() ?>
--- templates/layout.php
+++ templates/layout.php
@@ -88,6 +88,7 @@ function nav ( $current = 'blog' ) {
   <div id="fb-root"></div>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
   <script>!window.jQuery && document.write('<script src="js/jquery-1.4.2.min.js"><\/script>')</script>
+  <script src="js/jquery-effects-core.min.js"></script>
   <script src="js/plugins.js?v=1"></script>
   <script src="js/script.js?v=1"></script>
   <script src="http://connect.facebook.net/en_US/all.js"></script>
