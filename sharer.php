3af13ee6ce7a497f283e2781308a6f9576416d62
Tucker Bickler <Tucker@w-central-232-116.public.utexas.edu>
2010-10-16 06:28:59 -0500

    fixed merges, style, sharer

--- sharer.php
+++ sharer.php
@@ -2,11 +2,17 @@
 
 $u = $_GET['u'];
 $t = $_GET['t'];
-include 'templates/layout.php';
 
-startblock('title'); echo $t; endblock();
+?>
 
-startblock('content'); ?>
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+  <meta charset="utf-8">
+  <title>clippFeed - <?php echo $t; ?></title>
+</head>
+<body>
+
 <div id="sharer">
     <label for="new_url">New URL</label>
     <input id="new_url" name="url" type="text" value="<?php echo $u; ?>" />
@@ -14,8 +20,11 @@ startblock('content'); ?>
         hello
     </div>
 </div>
-<?php endblock();
 
-startblock('scriptTag'); ?>
-    <script src="js/preview.js"></script>
-<?php endblock(); ?>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script>!window.jQuery && document.write('<script src="js/jquery-1.4.2.min.js"><\/script>')</script>
+<script src="js/jquery-effects-core.min.js"></script>
+<script src="js/preview.js"></script>
+
+</body>
+</html>
