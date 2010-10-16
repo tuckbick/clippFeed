6eb53f419c8670f761aa7cee32cebbae4e6b3caf
Paul Marbach <fastfrwrd@wireless-128-62-82-75.public.utexas.edu>
2010-10-16 07:09:44 -0500

    add works mahhhvelously

--- js/preview.js
+++ js/preview.js
@@ -16,40 +16,46 @@ validate = function( url ) {
 $url = $('#new_url'),
 $preview = $('#new_url_preview'),
 $form = $('#add_clip'),
-lastTime = 0;
+lastTime = 0,
+updatePreview = function() {
+	var url = $url.val(),
+		type = validate( url );
+	if( type != -1 ) {
+		$.ajax({
+			url: 'handler.php',
+			dataType: 'json',
+			cache: false,
+			data: {
+				action: 'preview',
+				sid: type,
+				url: url
+			},
+			success: function( data ) {
+				if( data.hasOwnProperty('embed') ) {
+					if( data.embed != '' ) {
+						$preview.slideDown('fast', function(){$preview.html(data.embed);})
+					} else {
+						$preview.html('').delay(20).slideUp('fast');
+					}
+				}
+			},
+			error: function( ) {
+				$preview.html('').slideUp('fast');
+			}
+		});
+	}
+};
+$url.focus(function() {
+	if($url.val()=='paste a URL of a video and click add') {
+		$url.val('');
+	}
+});
+updatePreview();
 $url.bind('keyup paste', function(e) {
     var newTime = (new Date()).getTime();
     if( newTime - lastTime > 500 ) {
         lastTime = newTime;
-        var finish = function() {
-            var url = $url.val(),
-                type = validate( url );
-            if( type != -1 ) {
-                $.ajax({
-                    url: 'handler.php',
-                    dataType: 'json',
-                    cache: false,
-                    data: {
-                        action: 'preview',
-                        sid: type,
-                        url: url
-                    },
-                    success: function( data ) {
-                        if( data.hasOwnProperty('embed') ) {
-                            if( data.embed != '' ) {
-                                $preview.slideDown('fast', function(){$preview.html(data.embed);})
-                            } else {
-                                $preview.html('').delay(20).slideUp('fast');
-                            }
-                        }
-                    },
-                    error: function( ) {
-                        $preview.html('').slideUp('fast');
-                    }
-                });
-            }
-        }
-        setTimeout(finish,50);
+        setTimeout(updatePreview,50);
     }
 });
 
@@ -66,7 +72,8 @@ $form.submit(function() {
 			url: url
 		},
 		success: function( data ) {
-			//populate list again
+			$url.val('');
+            $preview.html('').delay(20).slideUp('fast');
 		}
 	});
 	return false;
